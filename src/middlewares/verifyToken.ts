84b91eb26d02b677f597629bc5ee444513dfad23
SantiZai <santyagozaidan@gmail.com>
2024-02-19 11:03:24 -0300

    jwt middleware

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['auth-token'];
+  !token ?? res.status(401).json({ message: "Can't be found a token" });
+  try {
+    const verified = jwt.verify(token, 'secretkey');
+    res.send(verified);
+    next();
+  } catch (e) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
