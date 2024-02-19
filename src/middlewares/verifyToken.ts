5c3d63ad8a8325255731e83a04c522f9b19bdf40
SantiZai <santyagozaidan@gmail.com>
2024-02-19 11:04:04 -0300

    types

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['auth-token'];
+  const token = req.headers['auth-token'][0];
   !token ?? res.status(401).json({ message: "Can't be found a token" });
   try {
     const verified = jwt.verify(token, 'secretkey');
