db73f4fd481116ec5b6b9bd71f1be3f912585705
SantiZai <santyagozaidan@gmail.com>
2024-02-26 14:09:27 -0300

    implementing swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(4000);
+  
+  // enabling swagger for document the API
+  const config = new DocumentBuilder()
+    .setTitle("Courts reservations web app")
+    .setDescription("The courts reservations API web")
+    .setVersion("1.0")
+    .addTag("sports")
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document)
+
+  // enable cors for client requests
+  // TODO: accept petitions for urls verifieds
+  app.enableCors()
+
+  await app.listen(5000);
 }
 bootstrap();
